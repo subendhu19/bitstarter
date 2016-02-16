64a68fe525f058afdbed109594212a246d34d131
Ubuntu <ubuntu@ip-172-31-35-234.us-west-2.compute.internal>
2016-02-16 05:30:15 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 var express = require('express')
 var app = express()
-var content
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
-
-fs.readFileSync('./index.html','utf8', function read (err,data) {
-    if (err) 
-	throw err
-    content = data
-})
-
-console.log(content)
 
 app.get('/', function(request, response) {
   response.send("content")
